1013925fa9b59834907324ea55b92c4122c005a0
Tim Urista <timothy.urista@gmail.com>
2020-07-09 10:12:07 -0700

    add more logic to delete old files

--- src/handler.py
+++ src/handler.py
@@ -15,12 +15,13 @@ def make_github_commit():
     # repo.edit(has_wiki=False)
     repo = g.get_repo("timurista/market-volatility")
     # contents = repo.get_contents("src/fang_volatility.json", ref="test")
+
     isonow = datetime.datetime.now().isoformat()
     new_file_name = f"stock_scores/{isonow}___fang_volatility.json"
     contents = ""
     with open("fang_volatility.json", "r") as f:
         contents = json.load(f)
-    
+
     created_file = repo.create_file(
         new_file_name,
         f"Added stock volatility score for {isonow}",
@@ -28,7 +29,26 @@ def make_github_commit():
         branch="develop",
     )
     print(created_file)
-    title = "Update stock volatility scores for current timestamp: {isonow}"
+
+    # get a list of files
+    dir_contents = repo.get_dir_contents("fang_volatility")
+
+    file_count = 0
+    max_files = 2
+    for contents in dir_contents:
+        file_count += 1
+        if contents.name:
+            datepart = contents.name.split("___")
+            delta = datetime.datetime.fromisoformat(datepart) - datetime.datetime.now()
+            if delta > 30 or file_count > max_files:
+                repo.delete_file(
+                    contents.path,
+                    "(cleanup) delete an old file",
+                    contents.sha,
+                    branch="develop",
+                )
+
+    title = f"Update stock volatility scores for current timestamp: {isonow}"
     body = """
     Summary:
     Current stock prices are out of sync and need to be merged in
@@ -39,7 +59,7 @@ def make_github_commit():
     """
     pr = repo.create_pull(title=title, body=body, head="develop", base="master")
     print(pr)
-    # make a PR
+    # pr.create_review_comment("LGTM! :tada:", commit=created_file.sha, )
 
     res = pr.merge(commit_message=title, merge_method="squash")
     print(res)
